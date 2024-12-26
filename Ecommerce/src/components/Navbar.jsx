8c182d922c1578e9a72467fbb74831e823b9b951
gshubham05 <gshubham.05@gmail.com>
2024-12-26 20:35:39 +0530

    dropdown menu completed

--- Ecommerce/src/components/Navbar.jsx
+++ Ecommerce/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/frontend_assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
-
+  const [visible, setVisible] = useState(false)
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img className='w-36' src={assets.logo} alt="" />
@@ -41,6 +41,23 @@ const Navbar = () => {
           <img src={assets.cart_icon} alt="" className='w-5 min-w-5' />
           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+        <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+      </div>
+
+      {/* Sidebar Menu */}
+
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+        <div className="flex flex-col text-gray-600">
+          <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6' to='/'>Home</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6' to='/collection'>Collection</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6' to='/about'>About</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6' to='/contact'>Contact</NavLink>
+
+        </div>
       </div>
     </div>
 
