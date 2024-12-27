ad4f2e5946b786a6917e0d8715225456985d812c
gshubham05 <gshubham.05@gmail.com>
2024-12-27 10:12:44 +0530

    LatestCollection ProductItem Title created

---
+++ Ecommerce/src/context/ShopContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    const currency = "$";
+    const deliveryFee = 10;
+
+    const value = {
+        products,
+        currency,
+        deliveryFee
+    };
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
+
+export default ShopContextProvider;
